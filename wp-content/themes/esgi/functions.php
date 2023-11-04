fc82204a1ebec492438cc7fb369967af65d21cca
johnnyhelloworld <johnny58723826@hotmail.fr>
2023-11-04 15:44:18 +0100

    first part of the menu

--- wp-content/themes/esgi/functions.php
+++ wp-content/themes/esgi/functions.php
@@ -1,2 +1,21 @@
 <?php
-// Silence is golden.
+
+// Déclaration des emplacements de menu
+add_action('init', 'esgi_init');
+function esgi_init() {
+	register_nav_menus([
+        'primary_menu' => __('Menu principal', 'ESGI')
+    ]);
+}
+
+// Ajout des supports de thème
+add_action('after_setup_theme','esgi_after_setup_theme');
+function esgi_after_setup_theme(){
+	add_theme_support('custom-logo');
+}
+
+// Chargement de la feuille de style
+add_action('wp_enqueue_scripts', 'esgi_enqueue_assets');
+function esgi_enqueue_assets(){
+	wp_enqueue_style('main', get_stylesheet_uri());
+}
--- wp-content/themes/esgi/header.php
+++ wp-content/themes/esgi/header.php
@@ -9,5 +9,14 @@
 	<body <?php body_class(); ?>>
 	<?php wp_body_open(); ?>
 		<header>
-
+            <div class="container">
+                <?php if (has_nav_menu('primary_menu')) { 
+                    wp_nav_menu([
+                        'theme_location' => 'primary_menu',
+                        'container' => 'nav',
+                        'container_class' => 'main-menu'
+                    ]);
+                }
+                ?>
+            </div>
 		</header>
