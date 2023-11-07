aa2460f313dca967de37becf8a8e03f726fe8a17
thomasR92 <thomas.riehl92@gmail.com>
2023-11-08 00:07:11 +0100

    ajout du footer

--- wp-content/themes/esgi/footer.php
+++ wp-content/themes/esgi/footer.php
@@ -1,6 +1,56 @@
-        <footer>
+        <footer id="footer-id">	
 
+
+			<div class="footer-container">
+
+				<div class="footer-item">
+					<div class="container">
+						<div class="container-item">
+							<?= getFooterLogoESGI("footerLogo")?>
+						</div>
+
+						<div class="container-item" id="text-bottom-left">
+							2022 Figma Template by ESGI
+						</div>
+
+					</div>
+				</div>
+
+				<div class="footer-item">
+					<div class="container">
+						<div class="container-item">
+							<div class="container-contact">
+								<div class="contact">
+									<p><b>Manager</b></br></br></p>
+									<p>+33 1 53 31 25 23</p>
+									<p>ceo@company.com</p>
+								</div>
+								<div class="contact">
+									<p><b>CEO</b></br></br></p>
+									<p>+33 1 53 31 25 25</p>
+									<p>info@esgi.com</p>
+								</div>
+							</div>
+						</div>
+
+						<div class="container-item">
+							<div class="logo-container">
+								<div class="logo"><?= getLogoLinkedin("LinkedinLogo")?></div>
+								<div class="logo"><?= getLogoFacebook("facebookLogo")?></div>
+							</div>
+						</div>
+						
+					</div>
+				</div>
+
+
+			</div>
+
+ 
 		</footer>
 		<?php wp_footer(); ?>
 	</body>
 </html>
+
+
+
