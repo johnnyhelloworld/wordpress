4df9bacfdd6becc14df4bddedea2cb07f7eb4e4d
johnnyhelloworld <johnny58723826@hotmail.fr>
2023-11-08 23:42:49 +0100

    created second component Our Services

--- wp-content/themes/esgi/index.php
+++ wp-content/themes/esgi/index.php
@@ -2,6 +2,7 @@
 
 <main id="site-content">
     <?php get_template_part('template-parts/template-about-us'); ?>
+    <?php get_template_part('template-parts/template-services'); ?>
 </main>
 
 <?php get_footer() ?>
