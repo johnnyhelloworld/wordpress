dd4566776fe2bd17ae201cd773293d3bad55b42e
johnnyhelloworld <johnny58723826@hotmail.fr>
2023-11-11 22:57:04 +0100

    finish homepage

--- wp-content/themes/esgi/index.php
+++ wp-content/themes/esgi/index.php
@@ -1,6 +1,15 @@
 <?php get_header() ?>
 
 <main id="site-content">
+    <div class="intro">
+        <h1>A really professional structure for all your events!</h1>
+        <?php
+            $attachment_id = 18;
+            echo '<div class="image1">';
+            echo wp_get_attachment_image($attachment_id, 'full');
+            echo '</div>';
+        ?>
+    </div>
     <?php get_template_part('template-parts/template-about-us'); ?>
     <?php get_template_part('template-parts/template-services'); ?>
     <?php get_template_part('template-parts/template-partners'); ?>
