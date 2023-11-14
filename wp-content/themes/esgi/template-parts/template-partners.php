bb7be5fd604052b536394c19f01c889f1dc43fb9
johnnyhelloworld <johnny58723826@hotmail.fr>
2023-11-14 23:04:57 +0100

    create template page Partners

--- wp-content/themes/esgi/template-parts/template-partners.php
+++ wp-content/themes/esgi/template-parts/template-partners.php
@@ -1,6 +1,6 @@
 <article id="our-partners">
     <div class="our-services">
-        <h2>Our Partners</h2>
+        <h2>Our Partners.</h2>
     </div>
 
     <div class="logos">
@@ -13,13 +13,14 @@
                 return 'Logo not found'; // Display an error message if the URL is empty or invalid
             }
         }
-
+        echo '<div class="logo-partner-images">';
         echo '<div class="logoPartner1">' . getPartnerLogo('partner_logo_1') . '</div>';
         echo '<div class="logoPartner2">' . getPartnerLogo('partner_logo_2') . '</div>';
         echo '<div class="logoPartner3">' . getPartnerLogo('partner_logo_3') . '</div>';
         echo '<div class="logoPartner4">' . getPartnerLogo('partner_logo_4') . '</div>';
         echo '<div class="logoPartner5">' . getPartnerLogo('partner_logo_5') . '</div>';
         echo '<div class="logoPartner6">' . getPartnerLogo('partner_logo_6') . '</div>';
+        echo '</div>'
         ?>
     </div>
 
